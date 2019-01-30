9a73512a3ad6928ab219419156100d0c4e5b1198
mirul22 <amirulikmal.biz@gmail.com>
2019-01-30 18:47:05 +0800

    updated JSX Rendering lists conditional rendering Handling events Updating the state

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import Movies from "./components/movies";
+import "./App.css";
 
 class App extends Component {
-  render() {
-    return (
-      <main className="container">
-      <h1>Hello World</h1>
-      </main>
-    );
-  }
+    render() {
+        return (
+            <main className="container">
+                <Movies />
+            </main>
+        );
+    }
 }
 
 export default App;
